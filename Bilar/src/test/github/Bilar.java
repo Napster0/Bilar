b30489dc2e66fc7cdd7dc93e7d94bd9ee3afdb2a
Kim <kim.alltrelen@yh.nackademin.se>
2012-11-15 10:01:31 +0100

    Second commit

---
+++ Bilar/src/test/github/Bilar.java
@@ -0,0 +1,15 @@
+package test.github;
+
+public class Bilar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Yeeeaahhh");
+
+	}
+
+}
